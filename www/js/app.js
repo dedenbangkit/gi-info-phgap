6897b793015029ef6bc7ab1a85409b8db1089928
dedenbangkit <mail@dedenbangkit.com>
2017-04-25 06:52:55 +0700

    screen

--- www/js/app.js
+++ www/js/app.js
@@ -11,19 +11,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    ionic.Platform.fullScreen();
-    if (window.StatusBar) {
-      return StatusBar.hide();
-    }
+
 
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
+    ionic.Platform.fullScreen();
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      return StatusBar.hide();
     }
   });
 })
